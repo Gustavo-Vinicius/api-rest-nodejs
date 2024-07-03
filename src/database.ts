cfe7bf4a84aec9ea72c997d8d45e54099a9fbd57
Gustavo-Vinicius <gustavovinicimos12345@gmail.com>
2024-07-03 11:19:13 -0300

    feat : Implementing environment variables and treating env with zod .

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,16 @@
+import 'dotenv/config'
 import { Knex, knex as setupKnex } from "knex";
+import { env } from './env';
+
+if(!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
     
         client: 'sqlite',
         connection: {
-            filename: './db/app.db',
+            filename: env.DATABASE_URL,
     
         },
     
